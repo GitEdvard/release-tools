147ade70872db3f225097895d346fede11529c61
Steinar Sturlaugsson <steinar.sturlaugsson@medsci.uu.se>
2015-11-11 17:55:43 +0100

    Install on Windows
    - Very much a quick fix so that this can be used right away, will be a
    proper package later

---
+++ install.bat
@@ -0,0 +1,7 @@
+@echo off
+REM This is a temporary hack - will be installed properly later
+REM Ensure that you've got Python >= 2.7.9
+
+python -m pip install click
+python -m pip install requests
+
