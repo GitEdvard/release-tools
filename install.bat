599ff30970a28a6fbac19317a0086f37081417f5
Steinar Sturlaugsson <withrocks@users.noreply.github.com>
2015-11-21 23:21:13 +0100

    Remove install.bat

--- install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM This is a temporary hack - will be installed properly later
-REM Ensure that you've got Python >= 2.7.9
-
-python -m pip install click
-python -m pip install requests
-
